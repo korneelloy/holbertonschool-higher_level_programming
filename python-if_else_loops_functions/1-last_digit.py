5e4a1a1e345fb5da6cd84d228c6716a043b9b48a
korneel <korneel.loy@gmail.com>
2025-01-13 15:40:34 +0100

    Python if ex 1, handling neg numbers

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 last = abs(number) % 10
+if number < 0:
+    last = 0 - last
 phrase = "Last digit of " + str(number) + " is " + str(last) + " and is"
 if last > 5:
     print(f"{phrase} greater than 5")
