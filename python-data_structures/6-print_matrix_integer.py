b005cc16b6dae6112f17c6b501ab10b5c24bd213
korneel <korneel.loy@gmail.com>
2025-01-20 14:23:44 +0100

    python data structures ex 6 last white space takken out

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if len(matrix[0]) == 0:
+        print("")
     for sublist in matrix:
-        for el in sublist:
-            print("{:d}".format(el), end=' ')
-        print("".format())
+        for i in range(len(sublist)):
+            if (i == len(sublist) - 1):
+                print("{:d}".format(sublist[i]))
+            else:
+                print("{:d}".format(sublist[i]), end=" ")
