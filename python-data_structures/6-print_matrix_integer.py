53d4d210668df7095e808ec4cb1e27726c212ab0
korneel <korneel.loy@gmail.com>
2025-01-20 12:32:39 +0100

    python data structures ex 6 format added for requirement issues

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     for sublist in matrix:
         for el in sublist:
             print("{:d}".format(el), end=' ')
-        print()
+        print("".format())
