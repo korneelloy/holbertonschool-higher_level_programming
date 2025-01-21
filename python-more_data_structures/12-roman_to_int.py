20cfdb5f9148378bbdcfa9fd0d9dc1516077eea9
korneel <korneel.loy@gmail.com>
2025-01-21 16:07:55 +0100

    python more data structures ex 12 is instead of == and some form ajustments

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
+    if not roman_string or type(roman_string) is str:
         return None
 
     combi = {
-    "I" : 1,
-    "V" : 5,
-    "X" : 10,
-    "L" : 50,
-    "C" : 100,
-    "D" : 500,
-    "M" : 1000
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
     }
     result = 0
     last = 0
