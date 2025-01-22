9c1262989fbca4eec53f0543dc94bcb348c42246
korneel <korneel.loy@gmail.com>
2025-01-22 10:43:58 +0100

    ex 0 break

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if x <= 0:
-        return 0
+    counter = 0
     for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end='')
+            counter += 1
         except IndexError:
-            print('')
-            return i
+            break
     print('')
-    return i + 1
+    return counter
--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
 def raise_exception():
+    try:
+        raise ValueError
+    except ValueError:
