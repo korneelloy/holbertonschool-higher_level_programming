a42ffcdefb5c3823ddd54dc1cb68b0111ac5b2f8
korneel <korneel.loy@gmail.com>
2025-01-23 14:54:43 +0100

    python test driven development ex 6

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    def test_max_integer(self):
+        self.assertAlmostEqual(max_integer([1, 5, 4]), 5)
+        self.assertAlmostEqual(max_integer([]), None)
+        self.assertAlmostEqual(max_integer(['a', 'c', 'b']), 'c')
+
+    def test_nb_args(self):
+        self.assertRaises(TypeError, max_integer, [1, 3, 4, 2], [3, 4, 5])
