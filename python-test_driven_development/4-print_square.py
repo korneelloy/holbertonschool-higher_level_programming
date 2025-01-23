99c41985450af1a11a12a96a3cdbf7fb99801644
korneel <korneel.loy@gmail.com>
2025-01-23 11:09:57 +0100

    Python test driven development ex 4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 
 """
-The XXXXXX module handles 
+The print_square module prints a square of # of given size
 
-This is handled through the XXXXXXX function
+This is handled through the print_square function
 """
+
 
 def print_square(size):
     """
-    Desc xxxx
+    prints a square of # of given size
     Args:
-        xxxx: xxxxx
-        xxxx: txxxx
+        size: number of rows / columns
     Returns:
-        xxxxx
+        None
     """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for times in range(size):
+        print("#" * size)
