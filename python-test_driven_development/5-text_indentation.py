5b5eb6fd7e73792105a427107b2f5b97ffc3a459
korneel <korneel.loy@gmail.com>
2025-01-23 14:10:42 +0100

    python test driven development ex 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python3
 
 """
-The XXXXXX module handles 
+The text_indentation module handles
+printing 2 new lines after special characters
 
-This is handled through the XXXXXXX function
+This is handled through the text_indentation function
 """
+
 
 def text_indentation(text):
     """
-    Desc xxxx
+    Prints 2 new lines after special characters . ? :
     Args:
-        xxxx: xxxxx
-        xxxx: txxxx
+        text: a string
     Returns:
-        xxxxx
+        None
     """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    line = ""
+    for letter in text:
+        if letter in ('.', '?', ':'):
+            line = line + letter
+            line = line.strip()
+            print("{:s}".format(line))
+            print()
+            line = ""
+        else:
+            line = line + letter
+    if letter not in ('.', '?', ':'):
+        line = line.strip()
+        print("{:s}".format(line), end='')
