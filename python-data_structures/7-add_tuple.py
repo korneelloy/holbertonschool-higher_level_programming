bc2be45cfea1927444265df8465ab9c87f3a29ed
korneel <korneel.loy@gmail.com>
2025-01-24 12:07:04 +0100

    Python data structures ex 7 form ajustments

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -19,5 +19,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len_a == 0 and len_b == 1:
         return ((tuple_b[0]), 0)
     elif len_a == 0 and len_b == 0:
-        return (0, 0)    
-    
+        return (0, 0)
