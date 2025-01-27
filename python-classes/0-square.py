7cfdc030b384ef414378d7368d7270df26dc8d6c
korneel <korneel.loy@gmail.com>
2025-01-27 14:55:45 +0100

    Python classes ex 0

---
+++ python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+The 0-square module defines an empty class named square
+"""
+
+
+class Square:
+    """
+    definition of empty class square
+    """
+    def __init__(self):
+        pass
