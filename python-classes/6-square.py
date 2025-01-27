4e1e8d1e5e49d93d8f54a2a4374eccaba0d74305
korneel <korneel.loy@gmail.com>
2025-01-27 18:15:50 +0100

    Python classes ex 6 correction interpretation second position argument

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -30,7 +30,8 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
     @property
     def position(self):
@@ -41,7 +42,8 @@ class Square:
         if (not isinstance(value, tuple) and isinstance(value[0], int) and
            isinstance(value[1], int) and value[0] >= 0 and value[1] >= 0):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        else:
+            self.__position = value
 
     def area(self):
         """
@@ -61,7 +63,8 @@ class Square:
         if self.size == 0:
             print()
         else:
+            if self.position[1] != 0:
+                print("" * self.position[1])
             for _ in range(self.size):
-                if self.position[1] >= 0:
-                    print(" " * self.position[0], end="")
+                print(" " * self.position[0], end="")
                 print("#" * self.size)
