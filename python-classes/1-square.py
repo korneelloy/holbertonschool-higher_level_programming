ed5e2a0a1ec8588a17b7851e6e14c6ab50d56672
korneel <korneel.loy@gmail.com>
2025-01-27 15:06:45 +0100

    Python classes ex 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+The 0-square module defines an class named square
+"""
+
+
+class Square:
+    """
+    definition of class square with:
+    args:
+        size: size of the square
+    """
+    def __init__(self, size):
+        self.__size = size
