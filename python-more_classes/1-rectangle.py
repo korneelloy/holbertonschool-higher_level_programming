7c0fdbbd13c05590322e8019d7a49ae274e18c63
korneel <korneel.loy@gmail.com>
2025-01-28 14:15:35 +0100

    python more classes ex 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+The rectangle module defines an rectangle with width and height
+"""
+
+
+class Rectangle:
+    """
+    defenition of class rectangle
+    argv:
+        width (positif int): width of rectangle
+        height (positif int): height of rectangle
+    """
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
