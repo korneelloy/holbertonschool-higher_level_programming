2b84d3642ac3aabcff7556c24857ff1a521cd0d1
korneel <korneel.loy@gmail.com>
2025-02-03 18:53:52 +0100

    python inheritance ex 5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+The base_geometry module for now only creates empty class
+"""
+
+
+class BaseGeometry:
+    """
+    empty BaseGeometry class
+    """
+    def __init__(self):
+        pass
