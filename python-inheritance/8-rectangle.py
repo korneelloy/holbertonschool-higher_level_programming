08dabae05fc73a59d2dbec60f5e6326e8ac9170f
korneel <korneel.loy@gmail.com>
2025-02-04 10:19:44 +0100

    Python inheritance ex 8 with copy of the code, no import

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,5 +1,37 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+The base_geometry module for now only creates empty class
+with one empty method calculating the area, supposedly
+and one method for data validation
+"""
+
+
+class BaseGeometry:
+    """
+    empty BaseGeometry class
+    """
+    def __init__(self):
+        pass
+
+    """
+    method calculating the area
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    """
+    method validating data
+    argv:
+        name: the name of the attribute
+        value: the value of the attribute
+    """
+
+    def integer_validator(self, name, value):
+        if isinstance(value, bool) or not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
 
 """
 The rectangle module defines a rectangle
