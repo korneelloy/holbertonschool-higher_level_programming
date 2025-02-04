187bbd8976f37431b5923f64c4022767a80a61b9
korneel <korneel.loy@gmail.com>
2025-02-04 10:44:44 +0100

    Python inheritance ex 7 change verification int

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -27,7 +27,7 @@ class BaseGeometry:
     """
 
     def integer_validator(self, name, value):
-        if isinstance(value, bool) or not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
