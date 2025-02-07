f32429930ec44c674f709414ed1220d1375d6ada
korneel <korneel.loy@gmail.com>
2025-02-07 14:00:13 +0100

    Python inheritance ex 8 small changes

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -5,29 +5,7 @@ with one empty method calculating the area, supposedly
 and one method for data validation
 """
 
-
-class BaseGeometry:
-    """
-    empty BaseGeometry class
-    """
-    def area(self):
-        """
-        method calculating the area
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        method validating data
-        argv:
-            name: the name of the attribute
-            value: the value of the attribute
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 """
 The rectangle module defines a rectangle
@@ -39,6 +17,7 @@ class Rectangle(BaseGeometry):
     the Rectangle class
     """
     def __init__(self, width, height):
+        """ init method """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
