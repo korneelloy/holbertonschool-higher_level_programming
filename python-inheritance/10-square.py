1990c4abc406dab795ba2f4a1265ff0e46fd64ed
korneel <korneel.loy@gmail.com>
2025-02-07 14:09:01 +0100

    Python inheritance ex 10 import

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -5,61 +5,7 @@ with one empty method calculating the area, supposedly
 and one method for data validation
 """
 
-
-class BaseGeometry:
-    """
-    empty BaseGeometry class
-    """
-    def area(self):
-        """
-        method calculating the area
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        method validating data
-        argv:
-            name: the name of the attribute
-            value: the value of the attribute
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-"""
-The rectangle module defines a rectangle
-"""
-
-
-class Rectangle(BaseGeometry):
-    """
-    the Rectangle class
-    """
-    def __init__(self, width=0, height=0):
-        """
-        init method
-        """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """
-        method calculating the area
-        """
-        return (self.__width * self.__height)
-
-    def __str__(self):
-        """
-        string method to format print function
-        """
-        phrase = "[{}] {}/{}"\
-            .format(type(self).__name__, self.__width, self.__height)
-        return (phrase)
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
