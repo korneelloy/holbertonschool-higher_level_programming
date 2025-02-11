6975c897fd02788c4e6e2099cdae50d0f6bed9cd
korneel <korneel.loy@gmail.com>
2025-02-11 10:59:20 +0100

    Pythons input output ex 6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+"""json file to object"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """creates an Object from a JSON file"""
+    with open(filename, 'r', encoding='UTF-8') as file:
+        content = file.read()
+    return (json.loads(content))
