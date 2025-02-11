9a0dec297ab859815186eb5ad766de8a250b065f
korneel <korneel.loy@gmail.com>
2025-02-11 10:29:49 +0100

    Pythons input output ex 4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+"""json to string module"""
+
+import json
+
+
+def from_json_string(my_str):
+    """returns an object (Python data structure) represented by JSON string"""
+    return (json.loads(my_str))
