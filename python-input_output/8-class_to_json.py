c689403abc7f68ee87e29cda157b26c834842704
korneel <korneel.loy@gmail.com>
2025-02-11 14:11:50 +0100

    Python input output ex 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+"""class to json module"""
+
+
+def class_to_json(obj):
+    """ returns dictionary description with simple data structure
+     (list, dictionary, string, integer and boolean)
+     for JSON serialization of an object:
+    """
+    my_dict = {}
+    for key, value in obj.__dict__.items():
+        my_dict[key] = value
+    return my_dict
