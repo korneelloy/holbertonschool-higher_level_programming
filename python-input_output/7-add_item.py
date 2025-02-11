36089fb33437d13d4d039ebef5fcc18111a06c16
korneel <korneel.loy@gmail.com>
2025-02-11 13:50:31 +0100

    Python input output ex 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+"""adds all arguments to a Python list, and then save them to a file"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = 'add_item.json'
+try:
+    json_object = load_from_json_file(filename)
+except FileNotFoundError:
+    json_object = []
+for arg in sys.argv[1:]:
+    json_object.append(arg)
+save_to_json_file(json_object, filename)
