92cedf3ad42f7667b476c4fe27849eb6b73a93a8
korneel <korneel.loy@gmail.com>
2025-02-11 10:19:26 +0100

    Pythons input output ex 3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+"""object to json module"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """returns JSON representation of an object (string):"""
+    return (json.dumps(my_obj))
