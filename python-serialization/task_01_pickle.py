ee1fb252df3998adda017c05dd64d82a6d7a5b8e
korneel <korneel.loy@gmail.com>
2025-02-11 21:55:37 +0100

    python serialization ex 1

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,1 +1,42 @@
 #!/usr/bin/env python3
+
+"""pickle module"""
+
+import pickle
+
+
+class CustomObject:
+    """an object"""
+    def __init__(self, name, age, is_student):
+        """init method"""
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """print function"""
+        print("Name: {}\nAge: {}\nIs Student: {}"
+              .format(self.name, self.age, self.is_student))
+
+    def serialize(self, filename):
+        """
+        serialize current instance of object via pickle module
+        and save to filename
+        argv:
+            filename
+        """
+        if not isinstance(self, CustomObject):
+            return None
+        else:
+            with open(filename, 'wb') as file:
+                pickle.dump(self, file)
+
+    @classmethod
+    def deserialize(cls, filename):
+        """
+        load and return an instance of the CustomObjec using Pickle module
+        argv:
+            filename
+        """
+        with open(filename, 'rb') as file:
+            return (pickle.load(file))
