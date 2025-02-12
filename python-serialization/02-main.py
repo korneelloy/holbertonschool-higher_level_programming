f355ef4b821df542b0b60dc852905fd5ad57f37e
korneel <korneel.loy@gmail.com>
2025-02-12 18:22:30 +0100

    Python Serialization ex 2

---
+++ python-serialization/02-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+from task_02_csv import convert_csv_to_json
+
+csv_file = "dada.csv"
+convert_csv_to_json(csv_file)
+print(f"Data from {csv_file} has been converted to data.json")
--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+"""
+module reading data from one format (CSV)
+and converting it into another format (JSON)
+using serialization techniques.
+"""
+
+import csv
+import json
+
+
+def convert_csv_to_json(CSV_filename):
+    """
+    convert cvs to json
+    argv:
+        CVS file
+    """
+    try:
+        with open(CSV_filename, mode='r') as file:
+            reader = csv.DictReader(file)
+            diction = [row for row in reader]
+    except FileNotFoundError:
+        return False
+    with open('data.json', 'w') as file:
+        json.dump(diction, file)
+        return True
