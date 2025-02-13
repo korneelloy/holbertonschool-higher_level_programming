a3d6cfc003c2dc7d82790bddd9521c8d244bf97d
korneel <korneel.loy@gmail.com>
2025-02-13 10:15:38 +0100

    python input output ex 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+
+"""Pascal s Triangle"""
+
+
+def pascal_triangle(n):
+    """ returns a list of lists of integers
+    representing the Pascal s triangle"""
+    the_list = []
+    prev_sublist = []
+    next_sublist = []
+    if n <= 0:
+        return the_list
+    else:
+        next_sublist = [1]
+        the_list.append(next_sublist)
+        if n == 1:
+            return the_list
+        else:
+            prev_sublist = [1, 1]
+            the_list.append(prev_sublist)
+            if n == 2:
+                return the_list
+            else:
+                for times in range(n-2):
+                    next_sublist = []
+                    next_sublist.insert(0, 1)
+                    for index in range(1, times+2):
+                        value = prev_sublist[index] + prev_sublist[index-1]
+                        next_sublist.insert(index, value)
+                    the_list.append(next_sublist)
+                    next_sublist.insert(n, 1)
+                    prev_sublist = next_sublist.copy()
+
+                return the_list
