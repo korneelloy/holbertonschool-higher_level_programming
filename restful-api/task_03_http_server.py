093227708a06d32b08782cbf87475c337047ed12
korneel <korneel.loy@gmail.com>
2025-02-17 18:52:03 +0100

    restfull api ex 3 info path

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -21,6 +21,14 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
             data = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(data).encode("utf-8"))
 
+        elif self.path == "/info":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            data = {"version": "1.0",
+                    "description": "A simple API built with http.server"}
+            self.wfile.write(json.dumps(data).encode("utf-8"))
+
         elif self.path == "/status":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
@@ -31,7 +39,7 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"Not Found")
+            self.wfile.write(b"Endpoint not found")
 
 
 def run(server_class=HTTPServer, handler_class=SimpleAPIHandler, port=8000):
