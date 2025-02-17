80c6b1723a9cc3022416c31df7901abb5ef0e689
korneel <korneel.loy@gmail.com>
2025-02-17 15:05:50 +0100

    Restful api ex 2

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,37 +1,30 @@
 import requests
-import json
 import csv
 
 
+"""fetch posts"""
+
+
 def fetch_and_print_posts():
+    """fetch and print posts"""
     response = requests.get('https://jsonplaceholder.typicode.com/posts')
     code = response.status_code
-    print(code)
+    print(f"Status Code: {code}")
     if code == 200:
         json_ob = response.json()
-
-    for item in json_ob:
-        print(item['title'])
+        for item in json_ob:
+            print(item['title'])
 
 
 def fetch_and_save_posts():
+    """fetch and register posts"""
     response = requests.get('https://jsonplaceholder.typicode.com/posts')
     code = response.status_code
 
     if code == 200:
         json_ob = response.json()
-
-    list_dic = []
-
-    for item in json_ob:
-        dic = {
-            'id': item['id'],
-            'title': item['title'],
-            'body': item['body']
-        }
-        list_dic.append(dic)
-
-    with open('posts.csv', mode='w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
-        writer.writeheader()
-        writer.writerows(list_dic)
+        with open('posts.csv', mode='w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'],
+                                    extrasaction="ignore")
+            writer.writeheader()
+            writer.writerows(json_ob)
