11883f0295a81a11b957da7ef37888a61d26825c
korneel <korneel.loy@gmail.com>
2025-02-18 12:39:21 +0100

    restful api ex 4

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,53 @@
+from flask import Flask, jsonify, request
+
+"""Flask test module"""
+app = Flask(__name__)
+
+users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
+
+
+@app.route('/')
+def home():
+    # function to handle ('/) route. returns the view.
+    return "Welcome to the Flask API!"
+
+
+@app.route('/data/')
+def returnUsers():
+    # function to handle ('/data) route. returns the view.
+    names = [user["name"] for user in users.values()]
+    return jsonify(names)
+
+
+@app.route('/status/')
+def returnStatus():
+    # function to handle ('/status) route. returns the view.
+    return "OK"
+
+
+@app.route('/users/<username>')
+def returnUser(username):
+    # function to handle ('/users/<username>') route. returns the view.
+    if username in users:
+        return jsonify(users[username])
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+
+@app.route('/add_user/', methods=['POST'])
+def addUser():
+    # function to handle ('/add_user') route. returns the view.
+    new_user = request.get_json()
+    if new_user is not None:
+        username = new_user.get('username')
+        if username:
+            users[username] = new_user
+            return jsonify({"message": "User added", "user": new_user}), 201
+        else:
+            return ('{"error": "Username is required"}', 400)
+    else:
+        return ("Something went wrong", 400)
+
+
+if __name__ == "__main__":
+    app.run()
