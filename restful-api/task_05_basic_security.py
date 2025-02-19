8e260306462566340c952e82df985306853f19b5
korneel <korneel.loy@gmail.com>
2025-02-19 13:42:53 +0100

    restful api ex 5 first test

---
+++ restful-api/task_05_basic_security.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+
+"""
+Develop a Simple API using Python with Flask
+"""
+
+from flask import Flask, request, jsonify
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required
+
+
+app = Flask(__name__)
+
+auth = HTTPBasicAuth()
+
+app.config["JWT_SECRET_KEY"] = "klsq87!/xcjk<yyu65"
+
+jwt = JWTManager(app)
+
+
+users = {}
+
+"""BASIC PROTECTION"""
+
+
+@auth.verify_password
+def verify_password(username, password):
+    """password check with simple verification"""
+    if username in users and\
+       check_password_hash(users[username]["password"], password):
+        return username
+
+
+@app.route('/basic-protected', methods=['GET'])
+@auth.login_required
+def basic_protected():
+    """view returning with basic protection"""
+    return ("Basic Auth: Access Granted")
+
+
+"""TOKEN PROTECTION"""
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    """password check with JWT / token"""
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username in users and\
+       check_password_hash(users[username]["password"], password):
+        access_token = create_access_token(identity=username)
+        return jsonify(access_token=access_token)
+
+    return jsonify({"message": "Invalid credentials"}), 401
+
+
+@app.route('/jwt-protected', methods=['GET'])
+@jwt_required()
+def jwt_protected():
+    """view returning with jwt protection"""
+    return jsonify({"message": "JWT Auth: Access Granted"})
+
+
+if __name__ == '__main__':
+    app.run()
