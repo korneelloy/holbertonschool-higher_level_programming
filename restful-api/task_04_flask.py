7ae897c8938389f8f7d820b336cd68be72e9001b
korneel <korneel.loy@gmail.com>
2025-02-19 11:20:57 +0100

    resful api ex 4 some more changes

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -16,38 +16,36 @@ def home():
 
 
 @app.route('/data')
-def returnUsers():
+def data():
     """ function to handle ('/data) route. returns the view."""
-    names = [user["username"] for user in users.values()]
-    return jsonify(names)
+    return jsonify(list(users.keys()))
 
 
 @app.route('/status')
-def returnStatus():
+def status():
     """ function to handle ('/status) route. returns the view."""
     return "OK"
 
 
 @app.route('/users/<username>')
-def returnUser(username):
+def users(username):
     """ function to handle ('/users/<username>') route. returns the view """
-    if username in users:
-        return jsonify(users[username])
+    user = users.get(username)
+    if user:
+        return jsonify(user)
     else:
         return jsonify({"error": "User not found"}), 404
 
 
 @app.route('/add_user', methods=['POST'])
-def addUser():
+def add_user():
     """ function to handle ('/add_user') route. returns the view."""
     new_user = request.get_json()
-    if new_user is not None:
-        username = new_user.get('username')
-        if username:
-            users[username] = new_user
-            return jsonify({"message": "User added", "user": new_user}), 201
-        else:
-            return jsonify('{"error": "Username is required"}'), 400
+    username = new_user.get("username")
+    if not username:
+        return jsonify('{"error": "Username is required"}'), 400
+    else:
+        return jsonify({"message": "User added", "user": new_user}), 201
 
 
 if __name__ == "__main__":
