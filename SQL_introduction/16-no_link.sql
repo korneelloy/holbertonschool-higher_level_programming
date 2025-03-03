ed1e2c5f94e355058e1ef28f4aabb37e3c3cedc4
korneel <korneel.loy@gmail.com>
2025-03-03 16:21:37 +0100

    SQL ex 16 typo

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 -- script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
 SELECT score, `name` FROM second_table
-WHERE `name` is NOT NULL
+WHERE `name` IS NOT NULL
 ORDER BY `name`
