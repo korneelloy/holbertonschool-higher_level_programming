4b029aae428a3931771329c49283b5480224515f
korneel <korneel.loy@gmail.com>
2025-03-04 10:13:27 +0100

    SQL more ex 0 to 5

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,2 @@
+-- Write a script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+
---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,3 @@
+-- script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_us;
+CREATE TABLE IF NOT EXISTS cities;
