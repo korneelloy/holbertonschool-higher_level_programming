efa4eb2296ca935b7d88e7d9a5d5ad16619ad566
korneel <korneel.loy@gmail.com>
2025-03-04 16:08:07 +0100

    SQL more ex 13 order by added

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,4 +5,5 @@ INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
-GROUP BY tv_genres.name;
+GROUP BY tv_genres.name
+ORDER BY COUNT(*) DESC;
