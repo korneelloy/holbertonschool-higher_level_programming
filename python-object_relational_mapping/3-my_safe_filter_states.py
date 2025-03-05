39a915dfba4339fecc57efde2c2651e7090d5a7a
korneel <korneel.loy@gmail.com>
2025-03-05 14:40:25 +0100

    python ORM Ex 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+"""script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument."""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """main function to avoid execution when imported"""
+    username = sys.argv[1]
+    pw = sys.argv[2]
+    db = sys.argv[3]
+    state_name = sys.argv[4]
+
+    try:
+        connection = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=username,
+            password=pw,
+            database=db
+        )
+
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM states WHERE `name` = %s\
+           ORDER BY id".format(state_name)
+
+        cursor.execute(query, (state_name,))
+
+        statesWithStateName = cursor.fetchall()
+        for states in statesWithStateName:
+            print(states)
+
+    except MySQLdb.Error as e:
+        print(f"Error connecting to MySQL: {e}")
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
+
+if __name__ == "__main__":
+    main()
