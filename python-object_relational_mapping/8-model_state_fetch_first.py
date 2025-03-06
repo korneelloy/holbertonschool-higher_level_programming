09175e66cfb38bbd5c6ef1263791ff63675701df
korneel <korneel.loy@gmail.com>
2025-03-06 14:18:55 +0100

    python orm ex 8

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""script that lists 1st State objects
+from the database hbtn_0e_6_usa"""
+
+import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker
+import sys
+from model_state import Base, State
+
+
+def main():
+    """connection to db"""
+    username = sys.argv[1]
+    pw = sys.argv[2]
+    db = sys.argv[3]
+    query = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(username, pw, db)
+    engine = sa.create_engine(query)
+
+    """creation of session"""
+    Session = sessionmaker(bind=engine)
+
+    """using built in method of sessions"""
+    with Session() as session:
+        first_state = session.query(State).first()
+        print(f"{first_state.id}: {first_state.name}")
+
+
+if __name__ == "__main__":
+    main()
